386c05c439da7fff3d7e55bb3871383821807bf7
UrchenkoV <urchenko-1813@mail.ru>
2020-11-04 14:42:05 +0300

    Git init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './assets/css/main.css'
+import './assets/sass/main.scss'
+
+import './assets/js/main'
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fs = require('fs')
+
+const isDev = process.env.NODE_ENV === 'development'
+const PATHS = {
+    src: path.resolve(__dirname, './src'),
+    dist: path.resolve(__dirname, './dist'),
+    assets: 'assets/'
+}
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        app: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+    },
+    devServer: {
+        port: 3030,
+        hot: isDev,
+        overlay: true,
+        open: true,
+        //contentBase: path.resolve(__dirname, './dist'),
+    },
+    module: {
+        rules: [
+            {
+                // css, scss
+                test: /\.(scss|css)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            reloadAll: true
+                        },
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`,
+            minify: false,
+        }))
+    ],
+}
